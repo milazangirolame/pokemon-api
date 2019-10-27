04853eb01b0357f2d2aae8f3ff1b11cae5daeca9
Mila Zangirolame <milazangirolame@gmail.com>
2019-10-27 17:03:56 -0300

    adding comparison for pokemons with same attack points

--- api.rb
+++ api.rb
@@ -12,21 +12,27 @@ while true
 end
 puts "Pokemons: " + pokemons.join(", ")
 
-def get_attack(pokemon)
-    print "Buscando pontos do pokemon inserido..."
+
+def fetch_data(pokemon)
     uri = URI('https://pokeapi.co/api/v2/pokemon/')
     uri.path += pokemon
     res = Net::HTTP.get_response(uri)
     resposta = JSON.parse(res.body)
-    pokemons_attack = resposta['stats'][4]['base_stat']
-    puts "O ataque do #{pokemon} é #{pokemons_attack}"
-    return pokemons_attack
 end
-
+def get_attack(pokemon)
+    pokemon_attack = fetch_data(pokemon)['stats'][4]['base_stat']
+    return pokemon_attack
+end
+def get_hp(pokemon)
+    pokemon_hp = fetch_data(pokemon)['stats'][5]['base_stat']
+    return pokemon_hp
+end
 h = {}
 pokemons.each do |pokemon|
-  h["#{pokemon}"] = get_attack(pokemon)
+    h = h.merge({
+      pokemon => { :attack => get_attack(pokemon.downcase) , :hp =>get_hp(pokemon.downcase)}
+      })
 end
 
-resposta = h.sort_by{|k,v| v}
-puts "O pokemon mais fraco da sua lista é #{resposta[0][0].strip}"
+resposta = h.sort_by{|k,v| [v[:attack], v[:hp]]}
+puts "O pokemon mais fraco da sua lista é #{resposta[0][0]}"
